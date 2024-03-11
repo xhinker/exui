c8a8b2587fa48a1d7bac59a86e9d4b9c36c64c03
“Andrew <xhinker@hotmail.com>
2024-03-10 19:38:13 -0700

    add start shell

---
+++ start_exui.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/andrewzhu/storage_1t_2/github_repos/exui
+source /home/andrewzhu/storage_1t_2/github_repos/exui/exui_venv_p310/bin/activate
+python server.py -nb -host 0.0.0.0:8080
