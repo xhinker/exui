4cc34f863c17a04ad90a6d420e8839cccbeba210
“Andrew <xhinker@hotmail.com>
2024-04-22 21:30:53 -0700

    anzhu change

--- start_exui.sh
+++ start_exui.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/andrewzhu/storage_1t_2/github_repos/exui
-source /home/andrewzhu/storage_1t_2/github_repos/exui/exui_venv_p310/bin/activate
+#source /home/andrewzhu/storage_1t_2/github_repos/exui/exui_venv_p310/bin/activate
+source /home/andrewzhu/storage_1t_1/az_git_folder/azcode/az_projects/azgpt/exllamav2/exllamav2_venv_p310/bin/activate
 python server.py -nb -host 0.0.0.0:8080
